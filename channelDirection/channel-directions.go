bd0f7a21142654fb6cacc548d4cd20bfe10c5a7b
cuih <cuihang97@foxmail.com>
2021-06-01 17:45:14 +0800

    finish channels

---
+++ channelDirection/channel-directions.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func ping() {}
+
+func pong() {
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<- pongs)
+}
